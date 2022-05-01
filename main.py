8a8dc0a27b9d49e8d7b6737e91997bf9a03cf36f
DaNinjaBoi <ryu.oscar9@gmail.com>
2022-04-30 22:42:42 -0600

    after gym

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import cv2
 
 img = cv2.imread('assets/simp.jpg', 1)
 img = cv2.resize(img, (0, 0), fx=1, fy=1)
-img = cv2.rotate(img, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-cv2.imwrite('new_img.jpg', img)
+img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+
+#cv2.imwrite('new_img.jpg', img)
 
 cv2.imshow('Image', img)
 
