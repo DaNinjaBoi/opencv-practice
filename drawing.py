ab30a56a4d74c5f32bf0aef53ab54767eb7bc8b7
DaNinjaBoi <ryu.oscar9@gmail.com>
2022-06-09 15:51:40 -0600

    am done with this one

--- drawing.py
+++ drawing.py
@@ -12,6 +12,7 @@ while True:
     img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 10)
     img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5) #-1 line thickness for complete fill
     img = cv2.circle(img, (300, 300), 60, (0, 0, 255), -1)
+
     font = cv2.FONT_HERSHEY_SIMPLEX
     img = cv2.putText(img, 'Bossman', (200, height - 10), font, 1, (0, 0, 0), 5, cv2.LINE_AA)
 
