414dac4410ba354d848bc5043fca1f4e8841abc3
DaNinjaBoi <ryu.oscar9@gmail.com>
2022-06-11 14:15:15 -0600

    only blue but hsv hasnt been perfectly altered

--- color_detection.py
+++ color_detection.py
@@ -8,7 +8,15 @@ while True:
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    cv2.imshow('frame', frame)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([110, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', result)
 
     if cv2.waitKey(1) == ord('q'):
         break
