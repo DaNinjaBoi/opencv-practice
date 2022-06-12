1ba527d5714fc7af21086e325c48fb21d810ad68
DaNinjaBoi <ryu.oscar9@gmail.com>
2022-06-12 12:24:00 -0600

    space

--- corner_detection.py
+++ corner_detection.py
@@ -19,7 +19,6 @@ for i in range(len(corners)):
         color = tuple(map(lambda x: int(x), np.random.randint(0, 255, size=3)))
         cv2.line(img, corner1, corner2, color, 1)
 
-
 cv2.imshow('frame', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
